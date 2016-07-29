c54d361f1bd0af4e7a7b5edfa43b71499c66b6e5
Elif T. Kus <elifkus@gmail.com>
2016-07-29 15:45:27 +0300

    Implemented unpack_array

--- unpacker/src/unpacker/arrayunpacker.py
+++ unpacker/src/unpacker/arrayunpacker.py
@@ -7,6 +7,14 @@ def unpack_array(arr):
     '''
     Function
     '''
-    return
+    result = []
+    
+    for item in arr:
+        if type(item) is list:
+            intermediate_result = unpack_array(item) 
+            result.extend(intermediate_result)
+        else:
+            result.append(item) 
+    return result
     
     

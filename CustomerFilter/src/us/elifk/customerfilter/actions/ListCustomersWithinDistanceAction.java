72a187bccbdaa1d79b4801151221ab6660dcc263
Elif T. Kus <elifkus@gmail.com>
2016-08-01 20:33:43 +0300

    Removed warnings and unused tests

--- CustomerFilter/src/us/elifk/customerfilter/actions/ListCustomersWithinDistanceAction.java
+++ CustomerFilter/src/us/elifk/customerfilter/actions/ListCustomersWithinDistanceAction.java
@@ -1,7 +1,6 @@
 package us.elifk.customerfilter.actions;
 
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
--- CustomerFilter/src/us/elifk/customerfilter/conversion/JsonStringToObjectConvertor.java
+++ CustomerFilter/src/us/elifk/customerfilter/conversion/JsonStringToObjectConvertor.java
@@ -10,7 +10,6 @@ import us.elifk.customerfilter.data.Customer;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonParseException;
-import com.google.gson.JsonSyntaxException;
 
 public class JsonStringToObjectConvertor {
 	

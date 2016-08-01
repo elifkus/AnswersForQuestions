fdf0f6769906efe673ca741150cfa5307bbeb2f1
Elif T. Kus <elifkus@gmail.com>
2016-08-01 22:29:06 +0300

    Added tests for array_unpacker

--- unpacker/test/unpacker/unpacker_test.py
+++ unpacker/test/unpacker/unpacker_test.py
@@ -8,13 +8,41 @@ from unpacker import arrayunpacker
 
 class Test(unittest.TestCase):
 
-    #[[1,2,[3]],4] → [1,2,3,4]
-    def testName(self):
+    def testUnpackArray1(self):
         arr = [[1,2,[3]],4]
         output = arrayunpacker.unpack_array(arr)
         expected = [1,2,3,4]
         self.assertEqual(output, expected)
 
+    def testUnpackArray2(self):
+        arr = [1,2,3,4]
+        output = arrayunpacker.unpack_array(arr)
+        expected = [1,2,3,4]
+        self.assertEqual(output, expected)
+
+    def testUnpackArray3(self):
+        arr = []
+        output = arrayunpacker.unpack_array(arr)
+        expected = []
+        self.assertEqual(output, expected)
+
+    def testUnpackArray4(self):
+        arr = [[[[1], 2], 3], 4]
+        output = arrayunpacker.unpack_array(arr)
+        expected = [1,2,3,4]
+        self.assertEqual(output, expected)
+     
+    def testUnpackArray5(self):
+        arr = [1,[ 2, [3, [4]]]]
+        output = arrayunpacker.unpack_array(arr)
+        expected = [1,2,3,4]
+        self.assertEqual(output, expected)
+        
+    def testUnpackArray6(self):
+        arr = [1,[ 2, [3, [4]]], 1,[ 2, [3, [4]]], 1,[ 2, [3, [4]]], 1,[ 2, [3, [4]]], 1,[ 2, [3, [4]]]]
+        output = arrayunpacker.unpack_array(arr)
+        expected = [1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4]
+        self.assertEqual(output, expected)
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
